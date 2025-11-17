90be709c13691a445a68b22c06e2ce1d66f42426
belmegatron <rich@reveng.ai>
2025-11-17 19:29:49 +0000

    feat(PLU-192): attempting to retrieve data types for matched functions

--- reai_toolkit/app/services/data_types/data_types_service.py
+++ reai_toolkit/app/services/data_types/data_types_service.py
@@ -1,8 +1,10 @@
+import debugpy
 
 from revengai import (
     Configuration,
 )
 
+from revengai.models import MatchedFunction
 from reai_toolkit.app.core.netstore_service import SimpleNetStore
 from revengai import (
     FunctionsDataTypesApi,
@@ -13,21 +15,40 @@ from revengai import (
 from reai_toolkit.app.interfaces.thread_service import IThreadService
 from reai_toolkit.app.transformations.import_data_types import ImportDataTypes
 
+def wait_for_debugger():
+    # Start debug server
+    debugpy.listen(60000, in_process_debug_adapter=True)
+    print("Waiting for debugger attach...")
+    debugpy.wait_for_client()  # Pause until debugger connects
+    print("Debugger attached!")
+
 
 class ImportDataTypesService(IThreadService):
     def __init__(self, netstore_service: SimpleNetStore, sdk_config: Configuration):
         super().__init__(netstore_service=netstore_service, sdk_config=sdk_config)
 
-    def import_data_types(self, analysis_id: int, function_ids: list[int] | None = None):
-        # TODO: PLU-192 Create lookup of effective_address: matched_function
-        # TODO: PLU-192 Batch function_ids from the same analysis_id together before making the API call.
-        # TODO: PLU-192 Make the API call and import the types using the matched function data.
-        # TODO: PLU-192 Attempt to apply these but using the original effective address. This means we need a way of going back from matched function to src ea.
+    def import_data_types(self, matches: dict[int, MatchedFunction]):
+        wait_for_debugger()
+        idt : ImportDataTypes = ImportDataTypes()
 
-        response = self._get_data_types(analysis_id, function_ids)
-        if response:
-            idt : ImportDataTypes = ImportDataTypes()
-            idt.execute(response)
+        # Overwrite the matched effective address with the original effective address.
+        # We need this in order to ensure that we write the obtained function datatypes to the right location.
+        for original_ea, matched_func in matches.items():
+            matched_func.function_vaddr = original_ea
+
+        # Batch up function id's by analysis id in order to minimize the number of calls to the data types endpoint.
+        funcs_by_analysis_id: dict[int, list[int]] = {}
+        for matched_func in matches.values():
+            if funcs_by_analysis_id.get(matched_func.analysis_id) is None:
+                funcs_by_analysis_id[matched_func.analysis_id] = [matched_func.function_id]
+            else:
+                funcs_by_analysis_id[matched_func.analysis_id].append(matched_func.function_id)
+
+        # Attempt to retrieve the data types from the API and apply them to our analysis.
+        for analysis_id, function_ids in funcs_by_analysis_id.items():
+            response = self._get_data_types(analysis_id, function_ids)
+            if response:
+                idt.execute(response)
 
     def _get_data_types(self, analysis_id: int, function_ids: list[int] | None = None) -> FunctionDataTypesList | None:
         with self.yield_api_client(sdk_config=self.sdk_config) as api_client:
